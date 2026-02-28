1020ab2b89f05d65d314611acef363579f5bb69e
Erik Westlund <edbwestlund@gmail.com>
2026-02-28 16:18:41 -0500

    fix

--- config/nvim/lua/plugins/disable-cmp.lua
+++ config/nvim/lua/plugins/disable-cmp.lua
@@ -1,3 +1,22 @@
 return {
-  { "hrsh7th/nvim-cmp", enabled = false },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      -- Disable auto-completion for .env files
+      opts.enabled = function()
+        local ft = vim.bo.filetype
+        -- Disable for .env files and other sensitive config files
+        if ft == "env" or ft == "dotenv" then
+          return false
+        end
+        -- Also check filename patterns
+        local fname = vim.fn.expand("%:t")
+        if fname:match("^%.env") or fname:match("%.env%.") then
+          return false
+        end
+        return true
+      end
+      return opts
+    end,
+  },
 }
